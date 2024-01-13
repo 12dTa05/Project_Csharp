d9c08d2ed8701f3e435d8229fe8adbb48c5fbfee
12dTa05 <damanhqaz01@gmail.com>
2024-01-13 22:24:09 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System;
+
+
+class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new Exception("Access denied - You must be at least 18 years old.");
+        }
+        else
+        {
+            Console.WriteLine("Access granted - You are old enough!");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        checkAge(14);
+    }
+}
+
